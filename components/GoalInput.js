ac4317670f04c6c47ac8c7f3c1ec76ffc3fc9a45
shubham <singhshubham3006.ss@gmail.com>
2023-06-25 22:18:04 +0530

    Added goal image and change in background color

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -1,9 +1,9 @@
 // import React from 'react'
 
 import { useState } from "react"
-import { Button, StyleSheet, TextInput, View, Modal } from "react-native"
+import { Button, StyleSheet, TextInput, View, Modal, Image } from "react-native"
 
-
+const img = require('../assets/images/goal.png');
 
 
 function GoalInput(props) {
@@ -22,15 +22,17 @@ function GoalInput(props) {
     }
     return (
         <Modal visible={props.visible} animationType="slide">
+
             <View style={styles.inputContainer}>
+                <Image source={img} style={styles.image} />
                 <TextInput
                     style={styles.textInput}
                     placeholder="Your Goals!"
                     onChangeText={goalInputHandler}
                     value={enteredGoalText} />
                 <View style={styles.buttonContainer}>
-                    <Button style={styles.button} title="Add Goal" onPress={addGoalHandler} />
-                    <Button style={styles.button} title="Cancel" onPress={cancelHandler} />
+                    <Button style={styles.button} title="Cancel" onPress={cancelHandler} color='#f31282' />
+                    <Button style={styles.button} title="Add Goal" onPress={addGoalHandler} color='#b180f0' />
                 </View>
 
                 {/* if we directly call the addGoal function from props, enetredGoalText would not pass through from here to parent . to pass data from child to parent data, we use addGoalHandler of this module */}
@@ -44,13 +46,17 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
+        backgroundColor: '#311b6b',
     },
     textInput: {
         borderWidth: 2,
-        borderColor: "#CCCCCC",
+        borderColor: "#e4d0ff",
+        backgroundColor: "#e4d0ff",
         width: "90%",
+        borderRadius: 6,
         // marginRight: 8,
-        padding: 8,
+        padding: 16,
+        color: '#120438',
     },
     buttonContainer: {
         flexDirection: "row",
@@ -61,6 +67,11 @@ const styles = StyleSheet.create({
     button: {
         padding: 8,
     },
+    image: {
+        height: 100,
+        width: 100,
+        margin: 20,
+    }
 })
 
 export default GoalInput
